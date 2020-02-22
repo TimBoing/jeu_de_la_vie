4fa38b648f88ea06fe2c06ef9bceb4debedd7b08
Timothee Boingneres <timboing@icloud.com>
2020-02-22 17:05:19 +0100

    interval of 1 sec set

--- app/javascript/components/main_game.js
+++ app/javascript/components/main_game.js
@@ -1,7 +1,20 @@
 const myFunc = () => {
-  const myButton = document.getElementById('start-game')
+  const myButton = document.getElementById('start-game');
+  const cells = document.querySelectorAll('td');
+
+  const toggleThem = () => {
+    cells.forEach((cell) => {
+      cell.classList.toggle('alive');
+    });
+  }
+
+
   myButton.addEventListener(('click'), (event) => {
     alert("yo");
+  });
+
+  document.addEventListener("DOMContentLoaded", () => {
+    setInterval(toggleThem, 1000); // Every 1 second, the `refresh` function is called.
   });
 
 }
