56878e11699ee6a488ba2f18a61b3dc80754f710
Timothee Boingneres <timboing@icloud.com>
2020-02-22 16:56:30 +0100

    button working

---
+++ app/javascript/components/main_game.js
@@ -0,0 +1,9 @@
+const myFunc = () => {
+  const myButton = document.getElementById('start-game')
+  myButton.addEventListener(('click'), (event) => {
+    alert("yo");
+  });
+
+}
+
+export { myFunc };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,3 @@
 import "bootstrap";
+import { myFunc } from '../components/main_game';
+myFunc();
